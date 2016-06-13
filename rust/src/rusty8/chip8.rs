cd8fc348c197a37d03ad6f7522f56a18b236a04d
Marcus Brummer <mbrlabs7@gmail.com>
2016-06-14 00:32:07 +0200

    Decoding of all 35 opcodes done

--- rust/src/rusty8/chip8.rs
+++ rust/src/rusty8/chip8.rs
@@ -91,29 +91,150 @@ impl Chip8 {
         let opcode: u16 = (self.mem[self.pc] as u16) << 8 | (self.mem[self.pc + 1] as u16);
 
         // decode && execute
+        // Opcode list: https://en.wikipedia.org/wiki/CHIP-8#Opcode_table
         match opcode & 0xF000 {
             0x0 => {
-                match opcode & 0xFF00 {
-                    0x00E0 => {},
-                    0x00EE => {},
-                    _      => {/* 0x0NNN may be a candidate here */}
+                match opcode {
+                    0x00E0 => {
+                        // 00E0: Clears the screen
+                    },
+                    0x00EE => {
+                        // 00EE: Returns from a subroutine
+                    },
+                    _      => {
+                        // 0x0NNN: Calls RCA 1802 program at address NNN. 
+                        // Not necessary for most ROMs
+                    }
                 }
             },
-            0x1 => {},
-            0x2 => {},
-            0x3 => {},
-            0x4 => {},
-            0x5 => {},
-            0x6 => {},
-            0x7 => {},
-            0x8 => {},
-            0x9 => {},
-            0xA => {},
-            0xB => {},
-            0xC => {},
-            0xD => {},
-            0xE => {},
-            0xF => {},
+            0x1 => {
+                // 1NNN: Jumps to address NNN
+            },
+            0x2 => {
+                // 2NNN: Calls subroutine at NNN
+            },
+            0x3 => {
+                // 3XNN: Skips the next instruction if VX equals NN
+            },
+            0x4 => {
+                // 4XNN: Skips the next instruction if VX doesn't equal NN
+            },
+            0x5 => {
+                // 5XY0: Skips the next instruction if VX equals VY
+            },
+            0x6 => {
+                // 6XNN: Sets VX to NN
+            },
+            0x7 => {
+                // 7XNN: Adds NN to VX
+            },
+            0x8 => {
+                match opcode & 0x000F {
+                    0x0000 => {
+                        // 8XY0: Sets VX to the value of VY
+                    },
+                    0x0001 => {
+                        // 8XY1: Sets VX to VX or VY
+                    }, 
+                    0x0002 => {
+                        // 8XY2: Sets VX to VX and VY
+                    }, 
+                    0x0003 => {
+                        // 8XY3: Sets VX to VX xor VY
+                    }, 
+                    0x0004 => {
+                        // 8XY4: Adds VY to VX. VF is set to 1 when there's a carry, 
+                        // and to 0 when there isn't
+                    },
+                    0x0005 => {
+                        // 8XY5: VY is subtracted from VX. VF is set to 0 when there's 
+                        // a borrow, and 1 when there isn't
+                    }, 
+                    0x0006 => {
+                        // 8XY6: Shifts VX right by one. VF is set to the value of the 
+                        // least significant bit of VX before the shift
+                    }, 
+                    0x0007 => {
+                        // 8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a 
+                        // borrow, and 1 when there isn't
+                    }, 
+                    0x000E => {
+                        // 8XYE: Shifts VX left by one. VF is set to the value of the 
+                        // most significant bit of VX before the shift
+                    }, 
+                    _      => {/* Unsupported opcode */}
+                }
+            },
+            0x9 => {
+                // 9XY0: Skips the next instruction if VX doesn't equal VY
+            },
+            0xA => {
+                // ANNN: Sets I to the address NNN
+            },
+            0xB => {
+                // BNNN: Jumps to the address NNN plus V0
+            },
+            0xC => {
+                // CXNN: Sets VX to the result of a bitwise and operation on a 
+                // random number and NN
+            },
+            0xD => {
+                // DXYN: Sprites stored in memory at location in index register (I), 8bits 
+                // wide. Wraps around the screen. If when drawn, clears a pixel, register 
+                // VF is set to 1 otherwise it is zero. All drawing is XOR drawing 
+                // (i.e. it toggles the screen pixels). Sprites are drawn starting at position VX, VY. 
+                // N is the number of 8bit rows that need to be drawn. If N is greater than 1, second 
+                // line continues at position VX, VY+1, and so on
+            },
+            0xE => {
+                match opcode & 0x00FF {
+                    0x009E => {
+                        // EX9E: Skips the next instruction if the key stored in VX is pressed
+                    },
+                    0x00A1 => {
+                        // EXA1: Skips the next instruction if the key stored in VX isn't pressed
+                    },
+                    _      => {/* Unsupported opcode */}
+                }
+            },
+            0xF => {
+                match opcode & 0x00FF {
+                    0x0007 => {
+                        // FX07: Sets VX to the value of the delay timer
+                    }, 
+                    0x000A => {
+                        // FX0A: A key press is awaited, and then stored in VX
+                    }, 
+                    0x0015 => {
+                        // FX15: Sets the delay timer to VX
+                    },
+                    0x0018 => {
+                        // FX18: Sets the sound timer to VX
+                    },
+                    0x001E => {
+                        // FX1E: Adds VX to I
+                    },
+                    0x0029 => {
+                        // FX29: Sets I to the location of the sprite for the character in VX. 
+                        // Characters 0-F (in hexadecimal) are represented by a 4x5 font
+                    },
+                    0x0033 => {
+                        // FX33: Stores the binary-coded decimal representation of VX, with 
+                        // the most significant of three digits at the address in I, the middle 
+                        // digit at I plus 1, and the least significant digit at I plus 2. 
+                        // (In other words, take the decimal representation of VX, place the hundreds 
+                        // digit in memory at location in I, the tens digit at location I+1, and the 
+                        // ones digit at location I+2.)
+                    },
+                    0x0055 => {
+                        // FX55: Stores V0 to VX (including VX) in memory starting at address I
+                    },
+                    0x0065 => {
+                        // FX65: Fills V0 to VX (including VX) with values from memory starting at address I
+                    },
+                    _      => {/* Unsupported opcode */}
+                }
+            },
             _   => {},
         }
 
