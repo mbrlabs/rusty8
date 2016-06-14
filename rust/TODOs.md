e5e23274db24e192ee5c9b5ed8870aeff0b13936
Marcus Brummer <mbrlabs7@gmail.com>
2016-06-14 13:05:29 +0200

    Implemented 6XNN, 9XY0, ANNN, CXNN

---
+++ rust/TODOs.md
@@ -0,0 +1,36 @@
+## Opcodes
+0NNN 	
+00E0 	
+00EE 	
+1NNN 	
+2NNN 	
+3XNN 	
+3XNN 	
+5XY0 	
+6XNN 	[DONE]
+7XNN 	
+8XY0 	
+8XY1 	
+8XY2 	
+8XY3 	
+8XY4 	
+8XY5 	
+8XY6 	
+8XY7 	
+8XYE 	
+9XY0 	[DONE]
+ANNN 	[DONE]
+BNNN 	
+CXNN 	[DONE]
+DXYN 	
+EX9E 	
+EXA1 	
+FX07 	
+FX0A 	
+FX15 	
+FX18 	
+FX1E 	
+FX29 	
+FX33 	
+FX55 	
+FX65 	
--- rust/src/rusty8/chip8.rs
+++ rust/src/rusty8/chip8.rs
@@ -13,6 +13,8 @@
 // You should have received a copy of the GNU General Public License
 // along with rusty8. If not, see <http://www.gnu.org/licenses/>.
 
+extern crate rand;
+
 use rusty8::utils::Stack;
 
 const FONT: [u8; 80] = [
@@ -48,7 +50,7 @@ pub struct Chip8 {
     /// 15th register: carry flag, set if sprite is set from 1 to 0 (collision detection)
     v:          [u8; REGISTER_COUNT],
     /// Index register. Used to store addresses
-    i:          u8,
+    i:          u16,
     /// Program counter
     pc:         usize,
     /// Delay timer. Decrements at 60hz if set to a value > 0
@@ -97,86 +99,121 @@ impl Chip8 {
                 match opcode {
                     0x00E0 => {
                         // 00E0: Clears the screen
+                        // TODO implement
                     },
                     0x00EE => {
                         // 00EE: Returns from a subroutine
+                        // TODO implement
                     },
                     _      => {
                         // 0x0NNN: Calls RCA 1802 program at address NNN. 
                         // Not necessary for most ROMs
+                        // TODO implement
                     }
                 }
             },
             0x1 => {
                 // 1NNN: Jumps to address NNN
+                // TODO implement
             },
             0x2 => {
                 // 2NNN: Calls subroutine at NNN
+                // TODO implement
             },
             0x3 => {
                 // 3XNN: Skips the next instruction if VX equals NN
+                // TODO implement
             },
             0x4 => {
                 // 4XNN: Skips the next instruction if VX doesn't equal NN
+                // TODO implement
             },
             0x5 => {
                 // 5XY0: Skips the next instruction if VX equals VY
+                // TODO implement
             },
             0x6 => {
                 // 6XNN: Sets VX to NN
+                let register = (opcode & 0x0F00) >> 8;
+                self.v[register as usize] = (opcode & 0x00FF) as u8;
+                self.pc += 2;
             },
             0x7 => {
                 // 7XNN: Adds NN to VX
+                // TODO implement
             },
             0x8 => {
                 match opcode & 0x000F {
                     0x0000 => {
                         // 8XY0: Sets VX to the value of VY
+                        // TODO implement
                     },
                     0x0001 => {
                         // 8XY1: Sets VX to VX or VY
+                        // TODO implement
                     }, 
                     0x0002 => {
                         // 8XY2: Sets VX to VX and VY
+                        // TODO implement
                     }, 
                     0x0003 => {
                         // 8XY3: Sets VX to VX xor VY
+                        // TODO implement
                     }, 
                     0x0004 => {
                         // 8XY4: Adds VY to VX. VF is set to 1 when there's a carry, 
                         // and to 0 when there isn't
+                        // TODO implement
                     },
                     0x0005 => {
                         // 8XY5: VY is subtracted from VX. VF is set to 0 when there's 
                         // a borrow, and 1 when there isn't
+                        // TODO implement
                     }, 
                     0x0006 => {
                         // 8XY6: Shifts VX right by one. VF is set to the value of the 
                         // least significant bit of VX before the shift
+                        // TODO implement
                     }, 
                     0x0007 => {
                         // 8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a 
                         // borrow, and 1 when there isn't
+                        // TODO implement
                     }, 
                     0x000E => {
                         // 8XYE: Shifts VX left by one. VF is set to the value of the 
                         // most significant bit of VX before the shift
+                        // TODO implement
                     }, 
                     _      => {/* Unsupported opcode */}
                 }
             },
             0x9 => {
                 // 9XY0: Skips the next instruction if VX doesn't equal VY
+                // TODO implement 
+                let vx = (opcode & 0x0F00) >> 8;
+                let vy = (opcode & 0x00F0) >> 4;
+                if self.v[vx as usize] != self.v[vy as usize] {
+                    self.pc += 2;
+                }
+                self.pc += 2;
             },
             0xA => {
                 // ANNN: Sets I to the address NNN
+                self.i = opcode & 0x0FFF;
+                self.pc += 2;
             },
             0xB => {
                 // BNNN: Jumps to the address NNN plus V0
+                // TODO implement
             },
             0xC => {
                 // CXNN: Sets VX to the result of a bitwise and operation on a 
                 // random number and NN
+                let register = (opcode & 0x0F00) >> 8;
+                let num = rand::random::<u8>() & (opcode & 0x00FF) as u8;
+                self.v[register as usize] = num;
+                self.pc += 2;
             },
             0xD => {
                 // DXYN: Sprites stored in memory at location in index register (I), 8bits 
@@ -185,14 +222,17 @@ impl Chip8 {
                 // (i.e. it toggles the screen pixels). Sprites are drawn starting at position VX, VY. 
                 // N is the number of 8bit rows that need to be drawn. If N is greater than 1, second 
                 // line continues at position VX, VY+1, and so on
+                // TODO implement
             },
             0xE => {
                 match opcode & 0x00FF {
                     0x009E => {
                         // EX9E: Skips the next instruction if the key stored in VX is pressed
+                        // TODO implement
                     },
                     0x00A1 => {
                         // EXA1: Skips the next instruction if the key stored in VX isn't pressed
+                        // TODO implement
                     },
                     _      => {/* Unsupported opcode */}
                 }
@@ -201,22 +241,28 @@ impl Chip8 {
                 match opcode & 0x00FF {
                     0x0007 => {
                         // FX07: Sets VX to the value of the delay timer
+                        // TODO implement
                     }, 
                     0x000A => {
                         // FX0A: A key press is awaited, and then stored in VX
+                        // TODO implement
                     }, 
                     0x0015 => {
                         // FX15: Sets the delay timer to VX
+                        // TODO implement
                     },
                     0x0018 => {
                         // FX18: Sets the sound timer to VX
+                        // TODO implement
                     },
                     0x001E => {
                         // FX1E: Adds VX to I
+                        // TODO implement
                     },
                     0x0029 => {
                         // FX29: Sets I to the location of the sprite for the character in VX. 
                         // Characters 0-F (in hexadecimal) are represented by a 4x5 font
+                        // TODO implement
                     },
                     0x0033 => {
                         // FX33: Stores the binary-coded decimal representation of VX, with 
@@ -225,20 +271,23 @@ impl Chip8 {
                         // (In other words, take the decimal representation of VX, place the hundreds 
                         // digit in memory at location in I, the tens digit at location I+1, and the 
                         // ones digit at location I+2.)
+                        // TODO implement
                     },
                     0x0055 => {
                         // FX55: Stores V0 to VX (including VX) in memory starting at address I
+                        // TODO implement
                     },
                     0x0065 => {
                         // FX65: Fills V0 to VX (including VX) with values from memory starting at address I
+                        // TODO implement
                     },
                     _      => {/* Unsupported opcode */}
                 }
             },
-            _   => {},
+            _   => {/* Unsupported opcode */},
         }
 
-        // update timer registers
+        // TODO update timer registers
     }
 
     pub fn draw_requested(&self) -> bool {
